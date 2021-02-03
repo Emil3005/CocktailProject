ed2e1254f5c974865837cbdf2ed7fef414ad02ba
joossm <jooss.matthias@gmail.com>
2021-02-03 13:14:44 +0100

    Did something

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Building a Cocktail WebApp
         </a>
       </header>
     </div>
